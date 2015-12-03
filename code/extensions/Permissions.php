3d8426c76962fbecd8be1c97b46b886c6382f987
Andrew Aitken-Fincham <andrew@silverstripe.co.nz>
2015-12-04 12:20:15 +1300

    added limit by visible projects

--- code/extensions/Permissions.php
+++ code/extensions/Permissions.php
@@ -9,12 +9,19 @@ class Permissions extends \DNRoot implements \PermissionProvider {
 
 	const ALLOW_ENVIRONMENT_METRICS_READ = 'ALLOW_ENVIRONMENT_METRICS_READ';
 
-		/**
-	 * Return a map of permission codes to add to the dropdown shown in the Security section of the CMS.
-	 * array(
-	 *   'VIEW_SITE' => 'View the site',
-	 * );
-	 */
+	public function init() {
+		parent::init();
+
+		// Performs canView permission check by limiting visible projects
+		$project = $this->getCurrentProject();
+		if(!$project) {
+			return $this->project404Response();
+		}
+
+		if(!$project->allowed(self::ALLOW_ENVIRONMENT_METRICS_READ)) {
+			return \Security::permissionFailure();
+		}
+	}
 	public function providePermissions() {
 		return array(
 			self::ALLOW_ENVIRONMENT_METRICS_READ => array(
