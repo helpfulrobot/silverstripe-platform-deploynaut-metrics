b0d5f5c278965ca292f80cd1212d2ad0d6172749
Andrew Aitken-Fincham <andrew@silverstripe.co.nz>
2015-12-04 14:07:54 +1300

    remove init function

--- code/extensions/Permissions.php
+++ code/extensions/Permissions.php
@@ -5,23 +5,10 @@
 
 namespace DashboardMetrics;
 
-class Permissions extends \DNRoot implements \PermissionProvider {
+class Permissions implements \PermissionProvider {
 
 	const ALLOW_ENVIRONMENT_METRICS_READ = 'ALLOW_ENVIRONMENT_METRICS_READ';
 
-	public function init() {
-		parent::init();
-
-		// Performs canView permission check by limiting visible projects
-		$project = $this->getCurrentProject();
-		if(!$project) {
-			return $this->project404Response();
-		}
-
-		if(!$project->allowed(self::ALLOW_ENVIRONMENT_METRICS_READ)) {
-			return \Security::permissionFailure();
-		}
-	}
 	public function providePermissions() {
 		return array(
 			self::ALLOW_ENVIRONMENT_METRICS_READ => array(
